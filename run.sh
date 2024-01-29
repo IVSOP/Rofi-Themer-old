75b365b7653de0b50155ac0a12ae435e53d18df5
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2024-01-29 19:30:42 +0000

    Init

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="build"
+
+if cmake -S . -B $DIR; then
+	printf "\n"
+	if make -j$(nproc) -C $DIR; then
+		printf "\n"
+		./$DIR/Rofi-Themer-daemon
+	else
+		printf ">> build failed\n"
+	fi
+else
+	printf ">> configure failed\n"
+fi
